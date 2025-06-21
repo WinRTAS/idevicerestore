70aaa408155781eb39475adfb0f2bc975d619949
WinRTAS <winrsped@proton.me>
2025-06-21 14:43:54 +0000

    Small patch

--- src/asr.c
+++ src/asr.c
@@ -50,7 +50,7 @@
 int asr_open_with_timeout(idevice_t device, asr_client_t* asr, uint16_t port)
 {
 	int i = 0;
-	int attempts = 10;
+	int attempts = 100;
 	idevice_connection_t connection = NULL;
 	idevice_error_t device_error = IDEVICE_E_SUCCESS;
 
@@ -247,16 +247,17 @@ int asr_perform_validation(asr_client_t asr, ipsw_file_handle_t file)
 	while (1) {
 		if (asr_receive(asr, &packet) < 0) {
 			error("ERROR: Unable to receive validation packet\n");
-			return -1;
 		}
 
 		if (packet == NULL) {
-			if (attempts < 5) {
+			if (attempts < 10) {
 				info("Retrying to receive validation packet... %d\n", attempts);
 				attempts++;
 				sleep(1);
 				continue;
 			}
+			error("ERROR: Unable to receive validation packet\n");
+			return -1;
 		}
 
 		attempts = 0;
@@ -367,7 +368,7 @@ int asr_send_payload(asr_client_t asr, ipsw_file_handle_t file)
 	data = (char*)malloc(ASR_PAYLOAD_CHUNK_SIZE + 20);
 
 	i = length;
-	int retry = 3;
+	int retry = 10;
 	while(i > 0 && retry >= 0) {
 		uint32_t size = ASR_PAYLOAD_CHUNK_SIZE;
 		uint32_t sendsize = 0;
